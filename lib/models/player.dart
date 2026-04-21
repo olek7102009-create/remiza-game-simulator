e21e2ded4ae271b595dabbc64ff8fc712e421365
olek7102009-create <olek7102009@gmail.com>
2026-04-21 21:45:08 +0200

    Create Player class with attributes

---
+++ lib/models/player.dart
@@ -0,0 +1,17 @@
+class Player {
+  String name;
+  int health;
+  int stamina;
+  int level;
+  int points;
+  List<String> skills;
+
+  Player({
+    required this.name,
+    required this.health,
+    required this.stamina,
+    required this.level,
+    required this.points,
+    required this.skills,
+  });
+}
